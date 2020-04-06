538889d413004c001cacda042142deff27a7205b
Mingyu Kang <plenilune095@gmail.com>
2020-04-06 16:18:18 +0900

    표준입출력
    간단한 사칙연산 후 출력

---
+++ beakjoon/BaaaaaaaarkingDog/0x01/10869_사칙연산.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("%d\n", a + b);
+    printf("%d\n", a - b);
+    printf("%d\n", a * b);
+    printf("%d\n", a / b);
+    printf("%d\n", a % b);
+    
+    return 0;
+}
