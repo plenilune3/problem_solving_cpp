02499ab85036e5015887d9291fb6b559fcdfca49
Mingyu Kang <plenilune095@gmail.com>
2020-04-06 16:06:56 +0900

    표준입출력
    특수문자 출력하는 방법

---
+++ beakjoon/0x01/10171_고양이.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    printf("\\    /\\\n");
+    printf(" )  ( \')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|");
+
+    return 0;
+}
