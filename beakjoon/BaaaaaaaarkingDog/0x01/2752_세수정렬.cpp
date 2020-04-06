119f71ff0376d1451639c780353a5f8124f5e7b5
Mingyu Kang <plenilune095@gmail.com>
2020-04-06 17:27:09 +0900

    표준입출력
    간단한 정렬과 표준입출력

---
+++ beakjoon/BaaaaaaaarkingDog/0x01/2752_세수정렬.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a, b, c, temp;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a > b)
+    {
+        temp = a; a = b; b = temp;
+        printf("%d %d %d\n", a, b, c);
+    }
+    if (b > c)
+    {
+        temp = b; b = c; c = temp;
+        printf("%d %d %d\n", a, b, c);
+    }
+    if (a > b)
+    {
+        temp = a; a = b; b = temp;
+        printf("%d %d %d\n", a, b, c);
+    }
+    
+    printf("%d %d %d\n", a, b, c);
+    
+    return 0;
+}
