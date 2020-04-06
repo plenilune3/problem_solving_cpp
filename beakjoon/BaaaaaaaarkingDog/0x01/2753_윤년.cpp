ca64c0751199b615ffc12b5fec9256a401da89fe
Mingyu Kang <plenilune095@gmail.com>
2020-04-07 02:47:32 +0900

    표준입출력
    윤년인지 아닌지 판별

---
+++ beakjoon/BaaaaaaaarkingDog/0x01/2753_윤년.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int year, answer;
+
+    scanf("%d", &year);
+
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+                printf("1\n");   
+            else
+                printf("0\n");
+        }
+        else
+            printf("0\n");
+    }
+    else
+        printf("0\n");
+
+    return 0;
+}
