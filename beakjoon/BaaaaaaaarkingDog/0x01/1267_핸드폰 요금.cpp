42d185300307c6b628d670cc3ace9d726bfe3510
Mingyu Kang <plenilune095@gmail.com>
2020-04-07 12:44:03 +0900

    표준 입출력
    항상 문제의 조건을 자세히 읽고, 범위에 대한 부분을 생각해볼것

---
+++ beakjoon/BaaaaaaaarkingDog/0x01/1267_핸드폰 요금.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int Y = 0, M = 0, temp;
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &temp);
+
+        Y += ((temp / 30) + 1) * 10;
+        M += ((temp / 60) + 1) * 15;
+    }
+    printf("M %d\n", M);
+    printf("Y %d\n", Y);
+    if (Y == M) printf("Y M %d\n", Y);
+    else if (Y < M) printf("Y %d\n", Y);
+    else printf("M %d\n", M);
+
+    return 0;
+}
