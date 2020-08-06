41f501a2bf256ed1e71b3dd185430c1991815565
Mingyu Kang <plenilune095@gmail.com>
2020-08-07 00:27:05 +0900

    DFS
    사이클 찾는 문제

--- beakjoon/BaaaaaaaarkingDog/0x07/2668_숫자 고르기.cpp
+++ beakjoon/BaaaaaaaarkingDog/0x07/2668_숫자 고르기.cpp
@@ -1,30 +1,45 @@
 #include <iostream>
-#include <queue>
+#include <set>
 
 using namespace std;
 
 const int MAX = 101;
 
-int N;
+int N, startNode, endNode;
 int arr[MAX];
+int previous[MAX];
 bool visited[MAX];
 bool cycle[MAX];
-priority_queue<int> pq;
 
-void SelectNumber(int depth);
+void SelectNumber(int);
 
 int main(int argc, char const *argv[])
 {
+    set<int> answer;
     cin >> N;
 
     for (int i = 1; i <= N; i++)
         cin >> arr[i];
     
     for (int i = 1; i <= N; i++)
-        cout << arr[i] << " ";
-    cout << "\n";
+        if (visited[i] == false)
+        {
+            SelectNumber(i);
 
-    SelectNumber(0);
+            while (true)
+            {
+                answer.insert(endNode);
+                if (endNode == startNode)
+                    break;
+                endNode = previous[endNode];
+            }
+        }
+
+    cout << answer.size() << "\n";
+
+    for (set<int>::iterator iter = answer.begin(); iter != answer.end(); iter++)
+        cout << (*iter) << "\n";
+    cout << "\n";   
 
     return 0;
 }
@@ -36,10 +51,14 @@ void SelectNumber(int index)
     int nextNumber = arr[index];
 
     if (visited[nextNumber] == false)
-        SelectNumber(nextNumber);
-    else if (cycle[nextNumber] == 0)
     {
-
+        previous[nextNumber] = index;
+        SelectNumber(nextNumber);
+    }
+    else if (cycle[nextNumber] == false)
+    {
+        startNode = nextNumber;
+        endNode = index;
     }
 
     cycle[index] = true;
