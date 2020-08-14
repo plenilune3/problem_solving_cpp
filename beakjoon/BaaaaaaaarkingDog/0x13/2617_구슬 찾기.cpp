278ddfef1053fc7b445aa6260b8c4144572d9ceb
Mingyu Kang <plenilune095@gmail.com>
2020-08-14 17:51:31 +0900

    Floyd Warshall
    무거움을 나타내는 그래프와 가벼움을 나타내는 그래프를 이용하여
    각 그래프 마다 플로이드 와샬 알고리즘을 통해 거리를 구한다.
    무거운 구슬의 갯수나 가벼운 구슬의 갯수가 중간부분보다 커지면
    그 구슬은 무게가 중간일 수 없다.

---
+++ beakjoon/BaaaaaaaarkingDog/0x13/2617_구슬 찾기.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 101;
+
+int heavy[MAX][MAX];
+int light[MAX][MAX];
+
+int main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M, answer = 0;;
+    cin >> N >> M;
+
+    for (int i = 1; i <= N; i++)
+        for (int j = 1; j <= N; j++)
+            heavy[i][j] = light[i][j] = (i == j) ? 0 : MAX;
+    
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= N; j++)
+            cout << heavy[i][j] << " ";
+        cout << "\n";
+    }
+
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= N; j++)
+            cout << light[i][j] << " ";
+        cout << "\n";
+    }
+    while (M--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        heavy[a][b] = 1;
+        light[b][a] = 1;
+    }
+
+    for (int k = 1; k <= N; k++)
+        for (int i = 1; i <= N; i++)
+            for (int j = 1; j <= N; j++)
+            {
+                if (heavy[i][j] > heavy[i][k] + heavy[k][j])
+                    heavy[i][j] = heavy[i][k] + heavy[k][j];
+                
+                if (light[i][j] > light[i][k] + light[k][j])
+                    light[i][j] = light[i][k] + light[k][j];
+            }
+    
+
+    for (int i = 1; i <= N; i++)
+    {
+        int middle = (N + 1) / 2;
+        int heavy_cnt = 0, light_cnt = 0;
+
+        for (int j = 1; j <= N; j++)
+        {
+            if (heavy[i][j] != MAX) heavy_cnt++;
+            if (light[i][j] != MAX) light_cnt++;
+        }
+
+        if (heavy_cnt >= middle || light_cnt >= middle)
+            answer += 1;
+    }
+
+    cout << answer << "\n";
+
+    return 0;
+}
