2e78a37f9d01778efa41b2e1ffde3d0a9061a3e7
Mingyu Kang <plenilune095@gmail.com>
2020-09-25 21:30:33 +0900

    TopologySort
    기초적인 위상정렬 문제

---
+++ beakjoon/BaaaaaaaarkingDog/TopologySort/2252_줄세우기.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+const int MAX = 32001;
+
+int N, M;
+int in_degree[MAX];
+vector< vector<int> > node(MAX);
+
+vector<int> find_order();
+
+int main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    cin >> N >> M;
+
+    for (int i = 0; i < M; i++)
+    {
+        int u, v; cin >> u >> v;
+        node[u].push_back(v);
+        in_degree[v] += 1;
+    }
+
+    vector<int> answer = find_order();
+
+    for (vector<int>::iterator iter = answer.begin(); iter != answer.end(); iter++)
+        cout << (*iter) << " ";
+    cout << "\n";
+
+    return 0;
+}
+
+vector<int> find_order()
+{
+    vector<int> result;
+    queue<int> q;
+
+    for (int i = 1; i <= N; i++)
+        if (in_degree[i] == 0)
+            q.push(i);
+    
+    for (int i = 1; i <= N; i++)
+    {
+        int u = q.front();
+        q.pop();
+
+        result.push_back(u);
+
+        for (vector<int>::iterator iter = node[u].begin(); iter != node[u].end(); iter++)
+        {
+            int v = (*iter);
+
+            if (--in_degree[v] == 0)
+                q.push(v);
+        }
+    }
+
+    return result;
+}
+
