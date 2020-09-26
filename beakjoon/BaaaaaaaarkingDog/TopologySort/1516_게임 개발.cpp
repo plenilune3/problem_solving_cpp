16b4d5a8841ae488d51ab72e3c49b13c56cc472b
Mingyu Kang <plenilune095@gmail.com>
2020-09-26 13:41:35 +0900

    TopologySort
    선행순서가 있는 작업의 최소시간을 구할 때는 다이나믹 프로그래밍이 편하다.

--- beakjoon/BaaaaaaaarkingDog/TopologySort/1516_게임 개발.cpp
+++ beakjoon/BaaaaaaaarkingDog/TopologySort/1516_게임 개발.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <vector>
-#include <climits>
 
 using namespace std;
 
-const int MAX = 1e5 + 1;
+const int MAX = 501;
 
 int N;
+int build_time[MAX], build_memo[MAX];
 vector< vector<int> > node(MAX);
-int job_time[MAX], job_memo[MAX];
 
 int calc_time(int u);
 
@@ -17,43 +16,40 @@ int main(int argc, char const *argv[])
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
-    fill_n(&job_memo[0], MAX, -1);
+    fill_n(&build_memo[0], MAX, -1);
 
     cin >> N;
 
     for (int u = 1; u <= N; u++)
     {
-        int M;
-        cin >> job_time[u];
-        cin >> M;
+        cin >> build_time[u];
 
-        while (M--)
+        while (true)
         {
             int v; cin >> v;
+
+            if (v == -1)
+                break;
+            
             node[u].push_back(v);
         }
     }
 
-    int max_time = INT_MIN; 
-
     for (int u = 1; u <= N; u++)
-        max_time = max(max_time, calc_time(u));
-    
-    cout << max_time << "\n";
+        cout << calc_time(u) << "\n";
 
     return 0;
 }
 
 int calc_time(int u)
 {
-    if (job_memo[u] >= 0)
-        return job_memo[u];
+    if (build_memo[u] >= 0)
+        return build_memo[u];
     
     int max_time = 0;
 
     for (vector<int>::iterator iter = node[u].begin(); iter != node[u].end(); iter++)
         max_time = max(max_time, calc_time((*iter)));
     
-    return job_memo[u] = max_time + job_time[u];
+    return build_memo[u] = max_time + build_time[u];
 }
-
