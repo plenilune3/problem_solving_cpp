d204dbebfa0808b1301f6a9c9c7ade5aad25a079
Mingyu Kang <plenilune095@gmail.com>
2020-10-09 18:02:29 +0900

    Tree
    DFS + DP
    트리에서의 다이나믹 프로그래밍
    인접한 마을 중 우수 마을이 하나도 없을 경우
    그 마을은 2번 조건을 위배하지 않고 우수 마을로 선정할 수 있고,
    또한 선정하는 것이 이득이다.
    
    그래서 3번 조건은 고려하지 않고 2번 조건만 고려하면서
    최대합을 구해도 무방하다.

---
+++ beakjoon/BaaaaaaaarkingDog/Tree/1949_우수 마을.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int MAX = 1e4 + 1;
+
+int N;
+int villager[MAX], dp[MAX][2];
+bool visited[MAX];
+vector< vector<int> > node(MAX);
+vector< vector<int> > tree(MAX);
+
+void create_tree(int x);
+int get_excellent(int x, bool is_excellent);
+
+int main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    fill_n(&dp[0][0], MAX * 2, -1);
+
+    cin >> N;
+
+    for (int i = 1; i <= N; i++)
+        cin >> villager[i];
+    
+    for (int i = 0; i < N - 1; i++)
+    {
+        int u, v; cin >> u >> v;
+
+        node[u].push_back(v);
+        node[v].push_back(u);
+    }
+
+    create_tree(1);
+
+    int answer = max(get_excellent(1, 0), get_excellent(1, 1));
+
+    cout << answer << "\n";
+
+    return 0;
+}
+
+void create_tree(int x)
+{
+    visited[x] = true;
+
+    for (vector<int>::iterator iter = node[x].begin(); iter != node[x].end(); iter++)
+    {
+        int nx = (*iter);
+
+        if (visited[nx]) continue;
+
+        tree[x].push_back(nx);
+        create_tree(nx);
+    }
+}
+
+int get_excellent(int x, bool is_excellent)
+{
+    if (dp[x][is_excellent] != -1)
+        return dp[x][is_excellent];
+    
+    dp[x][is_excellent] = is_excellent ? villager[x] : 0;
+
+    for (vector<int>::iterator iter = tree[x].begin(); iter != tree[x].end(); iter++)
+    {
+        int nx = (*iter);
+
+        if (is_excellent)
+            dp[x][is_excellent] += get_excellent(nx, 0);
+        else
+            dp[x][is_excellent] += max(get_excellent(nx, 1), get_excellent(nx, 0));
+    }
+
+    return dp[x][is_excellent];
+}
+
+
