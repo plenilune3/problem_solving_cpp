12f604e2351dc01e57e3e9445f427b26d6d5d440
Mingyu Kang <plenilune095@gmail.com>
2020-10-25 23:44:13 +0900

    분류 변경

---
+++ beakjoon/BaaaaaaaarkingDog/Divide-and-Conquer/2448_별 찍기 - 11.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 3 * (1 << 10);
+
+int N;
+char board[MAX][MAX];
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
+
+void star(int x, int y, int l, char c)
+{
+
+}
