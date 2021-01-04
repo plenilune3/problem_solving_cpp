0b664c3a991efc8fda4f27e2ceb208a19df45d2c
Mingyu Kang <plenilune095@gmail.com>
2021-01-04 20:41:29 +0900

    SlidingWindow
    배열을 정렬하고 인접한 두 원소의 값으로
    정해진 구간의 차이의 최솟값을 구한다.
    
    M > 0, m > 0 일때, M + m 이 최솟값이 되려면 M과 m 둘 다 작아야 한다.
    이를 위해서 배열을 정렬한다.
    
    그리고 슬라이딩 윈도우를 통해 구간에서 차이의 최솟값을 찾는다.
    차이의 최대값은 쉽게 구할 수 있다.

--- beakjoon/BaaaaaaaarkingDog/SlidingWindow/3988_수 고르기.cpp
+++ beakjoon/BaaaaaaaarkingDog/SlidingWindow/3988_수 고르기.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
+#include <climits>
+#include <algorithm>
+#include <deque>
 
 using namespace std;
 
+const int MAX = 1e6 + 1;
+
+int N, K, answer = INT_MAX;
+int V[MAX];
+
 int main(int argc, char const *argv[])
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    cin >> N >> K;
+
+    for (int i = 0; i < N; i++)
+        cin >> V[i];
+    
+    sort(&V[0], &V[0] + N);
+
+    int T = N - K - 1;
+
+    deque< pair<int, int> > window;
+
+    for (int i = 1; i < N; i++)
+    {
+        int d = V[i] - V[i - 1];
+
+        while (!window.empty() && window.front().second > d)
+            window.pop_back();
+        
+        window.push_back(make_pair(i, d));
+
+        while (!window.empty() && window.front().first <= i - T)
+            window.pop_front();
+        
+        if (i >= T)
+            answer = min(answer, V[i] - V[i - T] + window.front().second);
+    }
+
+    cout << answer << "\n";
+
     return 0;
 }
