d76d92b953af869acd5f3322088a7ab090a72b0e
plenilune3 <plenilune095@gmail.com>
2021-04-20 02:42:53 +0900

    다시풀기
    달팽이 회전 구현
    움직임을 큐에 담아 놓으면 효율적으로 계산할 수 있음

--- beakjoon/삼성 SW 역량 테스트 기출 문제/20056_마법사 상어와 파이어볼.cpp
+++ beakjoon/삼성 SW 역량 테스트 기출 문제/20056_마법사 상어와 파이어볼.cpp
@@ -7,17 +7,19 @@ const int MAX = 51;
 const int dx[] = { -1, -1, 0, 1, 1, 1, 0, -1 };
 const int dy[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
 
-struct fire_ball
+struct fireball
 {
     int x, y, m, s, d;
+
+    fireball(int _x, int _y, int _m, int _s, int _d) : x(_x), y(_y), m(_m), s(_s), d(_d) {}
 };
 
 int N, M, K;
-queue<fire_ball> board[MAX][MAX];
+queue<fireball> A[MAX][MAX];
 
 void move();
-void divide();
-fire_ball make_fire_ball(int x, int y, int m, int s, int d);
+void spread();
+int sum_mass();
 
 int main(int argc, char const *argv[])
 {
@@ -28,27 +30,17 @@ int main(int argc, char const *argv[])
 
     while (M--)
     {
-        int x, y, m, s, d;
-        cin >> x >> y >> m >> s >> d;
-        board[x - 1][y - 1].push(make_fire_ball(x - 1, y - 1, m, s, d));
+        int x, y, m, s, d; cin >> x >> y >> m >> s >> d;
+        A[x - 1][y - 1].push(fireball(x - 1, y - 1, m, s, d));
     }
 
     while (K--)
     {
         move();
-        divide();
+        spread();
     }
 
-    int answer = 0;
-
-    for (int x = 0; x < N; x++)
-        for (int y = 0; y < N; y++)
-            while (!board[x][y].empty())
-            {
-                answer += board[x][y].front().m;
-                board[x][y].pop();
-            }
-    
+    int answer = sum_mass();
     cout << answer << "\n";
 
     return 0;
@@ -56,69 +48,75 @@ int main(int argc, char const *argv[])
 
 void move()
 {
-    queue<fire_ball> q;
+    queue<fireball> q;
 
     for (int x = 0; x < N; x++)
         for (int y = 0; y < N; y++)
-            while (!board[x][y].empty())
+            while (!A[x][y].empty())
             {
-                q.push(board[x][y].front());
-                board[x][y].pop();
+                q.push(A[x][y].front());
+                A[x][y].pop();
             }
-    
+
     while (!q.empty())
     {
-        fire_ball fb = q.front();
+        fireball fb = q.front();
         q.pop();
 
         int nx = (fb.x + dx[fb.d] * fb.s) % N;
         int ny = (fb.y + dy[fb.d] * fb.s) % N;
-
+        
         if (nx < 0) nx = (nx + N) % N;
         if (ny < 0) ny = (ny + N) % N;
 
-        board[nx][ny].push(make_fire_ball(nx, ny, fb.m, fb.s, fb.d));
+        A[nx][ny].push(fireball(nx, ny, fb.m, fb.s, fb.d));
     }
 }
 
-void divide()
+void spread()
 {
     for (int x = 0; x < N; x++)
         for (int y = 0; y < N; y++)
         {
-            if (board[x][y].size() < 2)
+            if (A[x][y].size() < 2)
                 continue;
+            
+            int m = 0, s = 0, c = 0;
+            bool odd = false, even = false;
 
-            int even_ = 0, odd_ = 0;
-            int mass = 0, speed = 0, count_ = 0;
-
-            while (!board[x][y].empty())
+            while (!A[x][y].empty())
             {
-                fire_ball fb = board[x][y].front();
-                board[x][y].pop();
-
-                count_ += 1;
-                mass += fb.m, speed += fb.s;
-
-                if (fb.d % 2) odd_ += 1;
-                else even_ += 1;
+                fireball fb = A[x][y].front();
+                A[x][y].pop();
+                m += fb.m, s += fb.s, c += 1;
+                fb.d % 2 ? odd = true : even = true;
             }
 
-            mass = mass / 5;
-            speed = speed / count_;
-
-            if (mass == 0)
+            if (m / 5 == 0)
                 continue;
-            
-            int temp = (even_ == count_ || odd_ == count_) ? 0 : 1;
-            
-            for (int i = temp; i < 8; i += 2)
-                board[x][y].push(make_fire_ball(x, y, mass, speed, i));
+
+            for (int i = 0; i < 8; i += 2)
+            {
+                if ((!odd && !even) || (odd && even))
+                    A[x][y].push(fireball(x, y, m / 5, s / c, i + 1));
+                else
+                    A[x][y].push(fireball(x, y, m / 5, s / c, i));
+            }
         }
 }
 
-fire_ball make_fire_ball(int x, int y, int m, int s, int d)
+int sum_mass()
 {
-    fire_ball fb = { x, y, m, s, d };
-    return fb;
+    int result = 0;
+    
+    for (int x = 0; x < N; x++)
+        for (int y = 0; y < N; y++)
+            while (!A[x][y].empty())
+            {
+                fireball fb = A[x][y].front();
+                A[x][y].pop();
+                result += fb.m;
+            }
+    
+    return result;
 }
--- beakjoon/삼성 SW 역량 테스트 기출 문제/20057_마법사 상어와 토네이토.cpp
+++ beakjoon/삼성 SW 역량 테스트 기출 문제/20057_마법사 상어와 토네이토.cpp
@@ -6,19 +6,17 @@ const int MAX = 500;
 const int dx[] = { 0, 1, 0, -1 };
 const int dy[] = { -1, 0, 1, 0 };
 
-int N;
-int board[MAX][MAX], answer;
+int N, A[MAX][MAX], answer;
 double S[5][5] = {
     { 0, 0, 0.02, 0, 0 },
     { 0, 0.1, 0.07, 0.01, 0 },
     { 0.05, 0, 0, 0, 0 },
-    { 0, 0.1, 0.07, 0.01, 0 },
-    { 0, 0, 0.02, 0, 0 }
+    { 0, 0.1, 0.07, 0.01, 0},
+    { 0, 0, 0.02, 0, 0}
 };
 
 void spread(int x, int y, int dir);
-void rotate_(double S[5][5]);
-bool is_out_of_range(int x, int y);
+void rotate(double S[5][5]);
 
 int main(int argc, char const *argv[])
 {
@@ -29,10 +27,10 @@ int main(int argc, char const *argv[])
 
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
-            cin >> board[i][j];
-        
+            cin >> A[i][j];
+    
     int x = N / 2, y = N / 2;
-    int k = 1, dir = 0, dist = 0, sw = -1;
+    int dist = 0, dir = 0, k = 1, sw = -1;
 
     while (true)
     {
@@ -42,21 +40,20 @@ int main(int argc, char const *argv[])
         {
             y += sw, k += 1;
             spread(x, y, dir);
-
-            if (k >= N * N) break;
         }
 
-        dir = (dir + 1) % 4; rotate_(S);
-
-        if (k >= N * N) break;
-
+        if (k >= N * N)
+            break;
+        
+        dir = (dir + 1) % 4; rotate(S);
+        
         for (int i = 0; i < dist; i++)
         {
             x -= sw, k += 1;
             spread(x, y, dir);
         }
 
-        dir = (dir + 1) % 4; rotate_(S); 
+        dir = (dir + 1) % 4; rotate(S);
 
         sw *= -1;
     }
@@ -68,46 +65,42 @@ int main(int argc, char const *argv[])
 
 void spread(int x, int y, int dir)
 {
-    int sum_ = 0;
+    int sum = 0;
 
     for (int i = 0; i < 5; i++)
         for (int j = 0; j < 5; j++)
         {
             if (S[i][j] == 0)
                 continue;
+            
+            int temp = A[x][y] * S[i][j];
+            sum += temp;
 
-            int temp = board[x][y] * S[i][j];
-            sum_ += temp;
-            int nx = x + i - 2, ny = y + j - 2;
+            int nx = x + (i - 2), ny = y + (j - 2);
 
-            if (is_out_of_range(nx, ny)) answer += temp;
-            else board[nx][ny] += temp;
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N)
+                answer += temp;
+            else
+                A[nx][ny] += temp;
         }
     
-    board[x][y] -= sum_;
+    A[x][y] -= sum;
     int nx = x + dx[dir], ny = y + dy[dir];
 
-    if (is_out_of_range(nx, ny)) answer += board[x][y];
-    else board[nx][ny] += board[x][y];
+    if (nx < 0 || nx >= N || ny < 0 || ny >= N)
+        answer += A[x][y];
+    else
+        A[nx][ny] += A[x][y];
     
-    board[x][y] = 0;
+    A[x][y] = 0;
 }
 
-void rotate_(double S[5][5])
+void rotate(double S[5][5])
 {
-    double S_copy[5][5];
+    double C[5][5];
+    copy(&S[0][0], &S[0][0] + 5 * 5, &C[0][0]);
 
     for (int i = 0; i < 5; i++)
         for (int j = 0; j < 5; j++)
-            S_copy[i][j] = S[j][5 - 1 - i];
-    
-    copy(&S_copy[0][0], &S_copy[0][0] + 5 * 5, &S[0][0]);
-}
-
-bool is_out_of_range(int x, int y)
-{
-    if (x < 0 || x >= N || y < 0 || y >= N)
-        return true;
-    
-    return false;
+            S[i][j] = C[j][5 - 1 - i];
 }
