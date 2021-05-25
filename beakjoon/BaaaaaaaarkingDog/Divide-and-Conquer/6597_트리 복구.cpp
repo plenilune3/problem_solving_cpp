84f67c10051337de3699d509f089afde3b9cbd6b
plenilune3 <plenilune095@gmail.com>
2021-05-25 23:54:52 +0900

    분할정복으로 플어야 하는 문제
    아직 힌트만 얻고 구현을 완성하지 못했음.

--- beakjoon/BaaaaaaaarkingDog/Divide-and-Conquer/6597_트리 복구.cpp
+++ beakjoon/BaaaaaaaarkingDog/Divide-and-Conquer/6597_트리 복구.cpp
@@ -4,17 +4,58 @@ using namespace std;
 
 const int MAX = 27;
 
-char P[MAX], I[MAX];
+string P, I;
+int N, idx[MAX];
+
+void get_postorder(int is, int ie, int ps, int pe);
 
 int main(int argc, char const *argv[])
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
-    while (cin >> P >> I)
-    {
-        cout << P << "\n";
-        cout << I << "\n";
-    }
+    cin >> P >> I;
+
+    N = (int) I.size();
+
+    for (int i = 0; i < N; i++)
+        idx[I[i] - 'A'] = i;
+
+    cout << N << "\n";
+
+    for (int i = 0; i < N; i++)
+        cout << idx[i] << " ";
+    cout << "\n";
+
+    // get_postorder(0, N - 1, 0, N - 1);
+
+    // while (cin >> P >> I)
+    // {
+    //     cout << P << "\n";
+    //     cout << I << "\n";
+
+    //     N = (int) I.size();
+
+    //     for (int i = 0; i < N; i++)
+    //         idx[I[i] - 'A'] = i;
+
+    //     get_postorder(0, N - 1, 0, N - 1);
+    // }
+
     return 0;
+}
+
+void get_postorder(int is, int ie, int ps, int pe)
+{
+    if (is > ie || ps > pe)
+        return;
+
+    int root_idx = idx[P[ps] - 'A'];
+    int left_size = root_idx - is;
+
+    cout << is << " " << ie << " " << ps << " " << pe << "\n";
+
+    get_postorder(is, root_idx - 1, ps, ps + left_size - 1);
+    get_postorder(root_idx + 1, ie, ps + left_size, pe - 1);
+    cout << I[root_idx] << " ";
 }
