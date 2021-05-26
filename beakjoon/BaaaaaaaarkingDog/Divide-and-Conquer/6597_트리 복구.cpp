d0d3ddfb559d020a387b4435ddd34071e513e64c
plenilune3 <plenilune095@gmail.com>
2021-05-26 20:04:52 +0900

    Divide-and-Conquer
    트리의 순회에 대해서 공부할 수 있는 문제
    인오더와 다른 하나의 오더를 알고 있으면
    남은 하나의 오더를 분할 정복으로 구할 수 있다.

--- beakjoon/BaaaaaaaarkingDog/Divide-and-Conquer/6597_트리 복구.cpp
+++ beakjoon/BaaaaaaaarkingDog/Divide-and-Conquer/6597_트리 복구.cpp
@@ -14,33 +14,16 @@ int main(int argc, char const *argv[])
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
-    cin >> P >> I;
+    while (cin >> P >> I)
+    {
+        N = (int) I.size();
 
-    N = (int) I.size();
+        for (int i = 0; i < N; i++)
+            idx[I[i] - 'A'] = i;
 
-    for (int i = 0; i < N; i++)
-        idx[I[i] - 'A'] = i;
-
-    cout << N << "\n";
-
-    for (int i = 0; i < N; i++)
-        cout << idx[i] << " ";
-    cout << "\n";
-
-    // get_postorder(0, N - 1, 0, N - 1);
-
-    // while (cin >> P >> I)
-    // {
-    //     cout << P << "\n";
-    //     cout << I << "\n";
-
-    //     N = (int) I.size();
-
-    //     for (int i = 0; i < N; i++)
-    //         idx[I[i] - 'A'] = i;
-
-    //     get_postorder(0, N - 1, 0, N - 1);
-    // }
+        get_postorder(0, N - 1, 0, N - 1);
+        cout << "\n";
+    }
 
     return 0;
 }
@@ -53,9 +36,7 @@ void get_postorder(int is, int ie, int ps, int pe)
     int root_idx = idx[P[ps] - 'A'];
     int left_size = root_idx - is;
 
-    cout << is << " " << ie << " " << ps << " " << pe << "\n";
-
-    get_postorder(is, root_idx - 1, ps, ps + left_size - 1);
-    get_postorder(root_idx + 1, ie, ps + left_size, pe - 1);
-    cout << I[root_idx] << " ";
+    get_postorder(is, root_idx - 1, ps + 1, ps + left_size);
+    get_postorder(root_idx + 1, ie, ps + left_size + 1, pe);
+    cout << I[root_idx];
 }
