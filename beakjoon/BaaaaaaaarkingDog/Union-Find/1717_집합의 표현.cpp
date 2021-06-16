d4318b580536de9058c51f4008e4ea4771d2f8a7
plenilune3 <plenilune095@gmail.com>
2021-06-16 23:52:09 +0900

    중간 저장

---
+++ beakjoon/BaaaaaaaarkingDog/Union-Find/1717_집합의 표현.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 1e6 + 1;
+
+int N, M;
+
+int main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    
+    cin >> N >> M;
+
+    while (M--)
+    {
+        int a, b, c; cin >> a >> b >> c;
+    }
+	return 0;
+}
