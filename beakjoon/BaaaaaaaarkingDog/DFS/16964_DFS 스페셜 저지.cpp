3bfe31f56e024be6e312341a16d70507f4d0ccea
plenilune3 <plenilune095@gmail.com>
2021-06-19 00:57:28 +0900

    DFS
    Set 자료구조를 사용하여 비교적 쉽게 풀었지만,
    스택을 이용하여 푸는 방법이 좀 더 일반적인 풀이인 것 같다.
    다시한번 풀어봐야 겠따.

--- beakjoon/BaaaaaaaarkingDog/DFS/16964_DFS 스페셜 저지.cpp
+++ beakjoon/BaaaaaaaarkingDog/DFS/16964_DFS 스페셜 저지.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <set>
 #include <vector>
 
 using namespace std;
 
 const int MAX = 1e5 + 1;
 
-int N;
+int N, idx;
+bool visited[MAX], possible = true;
 vector< vector<int> > node(MAX, vector<int> ());
+vector<int> order;
+
+void dfs(int u);
 
 int main(int argc, char const *argv[])
 {
@@ -23,5 +28,45 @@ int main(int argc, char const *argv[])
         node[v].push_back(u);
     }
 
+    for (int i = 1; i <= N; i++)
+    {
+        int x; cin >> x;
+        order.push_back(x);
+    }
+
+    if (order[0] != 1)
+    {
+        cout << 0 << "\n";
+        return 0;
+    }
+
+    dfs(1);
+
+    if (possible)
+        cout << 1 << "\n";
+    else
+        cout << 0 << "\n";
+
 	return 0;
+}
+
+void dfs(int u)
+{
+    set<int> s;
+    visited[u] = true;
+
+    for (vector<int>::iterator i = node[u].begin(); i != node[u].end(); i++)
+    {
+        int nv = (*i);
+        if (visited[nv]) continue;
+        s.insert(nv);
+    }
+
+    if (s.empty())
+        return;
+
+    if (s.find(order[idx + 1]) != s.end())
+        dfs(order[++idx]);
+    else
+        possible = false;
 }
