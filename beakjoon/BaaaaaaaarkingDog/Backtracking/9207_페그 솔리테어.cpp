9c271d02b5da0dc583b6bd67e2ed3f2a1beadca3
plenilune3 <plenilune095@gmail.com>
2021-06-25 23:54:42 +0900

    중간저장
    입력 처리하는게 더 까다로운 것 같다...

---
+++ beakjoon/BaaaaaaaarkingDog/Backtracking/9207_페그 솔리테어.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+const int MAX = 101;
+
+int N;
+char A[MAX][MAX];
+
+int main(int argc, char const *argv[])
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int T; cin >> T;
+	cout << T << "\n";
+
+	while (T--)
+	{
+		cout << "test" << "\n";
+	}
+
+	return 0;
+}
