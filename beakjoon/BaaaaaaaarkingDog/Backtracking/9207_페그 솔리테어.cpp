b70fd868bf96ef17ade81792f42b96866387bcaf
plenilune3 <plenilune095@gmail.com>
2021-06-27 23:51:39 +0900

    중간 저장

--- beakjoon/BaaaaaaaarkingDog/Backtracking/9207_페그 솔리테어.cpp
+++ beakjoon/BaaaaaaaarkingDog/Backtracking/9207_페그 솔리테어.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
-#include <cstring>
+#include <vector>
 
 using namespace std;
 
-const int MAX = 101;
+const int MAX = 8;
+const int dx[] = { -1, 1, 0, 0 };
+const int dy[] = { 0, 0, -1, 1 };
 
-int N;
-char A[MAX][MAX];
+const int N = 5;
+const int M = 9;
+
+int X;
+bool visited[MAX];
+char A[N][M];
+vector< pair<int, int> > P;
+vector<int> v;
+
+void play(int depth);
 
 int main(int argc, char const *argv[])
 {
@@ -14,12 +24,57 @@ int main(int argc, char const *argv[])
 	cin.tie(NULL), cout.tie(NULL);
 
 	int T; cin >> T;
-	cout << T << "\n";
 
 	while (T--)
 	{
-		cout << "test" << "\n";
+		P.clear();
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < M; j++)
+				cin >> A[i][j];
+		
+		X = (int) P.size();
+		cout << X << "\n";
+		play(0);
 	}
 
 	return 0;
+}
+
+bool is_possible(int x, int y, char A[N][M])
+{
+	if (A[x][y] != 'o')
+		return false;
+	
+	for (int i = 0; i < 4; i++)
+	{
+		int nx = x + dx[i], ny = y + dy[i];
+		int nnx = nx + dx[i], nny = ny + dy[i];
+		
+		if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+			continue;
+		if (nnx < 0 || nnx >= N || nny < 0 || nny >= M)
+			continue;
+		if (A[nx][ny] == 'o' && A[nnx][nny] == '.')
+			return true;
+	}
+	return false;
+}
+
+int count_possible(char A[N][M])
+{
+	int result = 0;
+
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+			result += is_possible(i, j, A);
+
+	return result;
+}
+
+void play(int depth)
+{
+	if (count_possible(A) == 0)
+	{
+		return;
+	}
 }
