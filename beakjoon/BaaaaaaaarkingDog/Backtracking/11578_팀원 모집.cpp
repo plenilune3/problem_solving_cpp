6dd4c37eb46831b44a121ce8f55a24c4b9a9d3c7
plenilune3 <plenilune095@gmail.com>
2021-07-03 23:57:46 +0900

    중간 저장

---
+++ beakjoon/BaaaaaaaarkingDog/Backtracking/11578_팀원 모집.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 10;
+
+int N, M;
+int P[MAX];
+
+int main(int argc, char const *argv[])
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	cin >> N >> M;
+
+	return 0;
+}
