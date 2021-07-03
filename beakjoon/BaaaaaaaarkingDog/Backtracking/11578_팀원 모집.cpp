c3d9bd553f195afae3dc4cf9e36495818188174b
plenilune3 <plenilune095@gmail.com>
2021-07-04 00:30:47 +0900

    Backtracking
    비트마스킹으로 탐색하는 횟수를 최대한 줄이는 것이 중요한 문제
    비트마스킹을 적용하면 탐색 시간을 줄일 수 있다.

--- beakjoon/BaaaaaaaarkingDog/Backtracking/11578_팀원 모집.cpp
+++ beakjoon/BaaaaaaaarkingDog/Backtracking/11578_팀원 모집.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <climits>
 
 using namespace std;
 
-const int MAX = 10;
+const int MAX = 11;
 
-int N, M;
-int P[MAX];
+int N, M, answer = INT_MAX, P[MAX];
+bool visited[MAX];
+
+void find_team(int depth, int index, int result);
 
 int main(int argc, char const *argv[])
 {
@@ -14,5 +17,44 @@ int main(int argc, char const *argv[])
 
 	cin >> N >> M;
 
+	for (int i = 0; i < M; i++)
+	{
+		int k; cin >> k;
+
+		for (int j = 0; j < k; j++)
+		{
+			int x; cin >> x;
+			P[i] = P[i] | (1 << (x - 1));
+		}
+	}
+
+	find_team(0, 0, 0);
+
+	if (answer != INT_MAX)
+		cout << answer << "\n";
+	else
+		cout << "-1" << "\n";
+
 	return 0;
+}
+
+void find_team(int depth, int index, int result)
+{
+	if (result == ((1 << N) - 1))
+	{
+		answer = min(answer, depth);
+		return;
+	}
+	if (depth == M)
+		return;
+	if (depth > answer)
+		return;
+	for (int i = index; i < M; i++)
+	{
+		if (visited[i]) continue;
+
+		visited[i] = true;
+		find_team(depth + 1, i, result | P[i]);
+		visited[i] = false;
+	}
 }
