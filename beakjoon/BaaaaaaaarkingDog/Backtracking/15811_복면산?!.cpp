324300ff50762624ffd690180719919d4b631f94
plenilune3 <plenilune095@gmail.com>
2021-07-09 23:58:06 +0900

    중간 저장

---
+++ beakjoon/BaaaaaaaarkingDog/Backtracking/15811_복면산?!.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 18;
+
+int main(int argc, char const *argv[])
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	return 0;
+}
